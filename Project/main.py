45f082a655739f1347365526c3b18f32d4261f02
Samil <naslam@windowslive.com>
2023-05-27 21:45:44 +0300

    changed with OpenWeather API with 3 days forecast. GUI can be improved.

--- Project/main.py
+++ Project/main.py
@@ -2,12 +2,14 @@ import tkinter as tk
 from tkinter import ttk
 import requests
 from bs4 import BeautifulSoup
+import os
 
 
 class WeatherApp:
     def __init__(self, master):
         self.master = master
         master.title("Weather App")
+
         master.configure(bg="lightblue")
 
         # Create city selection dropdown list
@@ -15,47 +17,62 @@ class WeatherApp:
         self.city_label.pack(padx=10, pady=10)
 
         self.city_var = tk.StringVar()
-        self.city_var.set("Izmir")
-        self.city_options = ["Izmir", "Istanbul", "Ankara", "Bursa", "Antalya", "Diyarbakir"]
-        self.city_dropdown = tk.OptionMenu(master, self.city_var, *self.city_options)
+        self.city_dropdown = ttk.Combobox(master, textvariable=self.city_var, font=("Helvetica", 14), state="readonly")
+        self.city_dropdown["values"] = ("Ankara", "Izmir", "Istanbul", "Antalya", "Bursa")
+        self.city_dropdown.current(0)
         self.city_dropdown.pack(padx=10, pady=10)
 
         # Create Get Weather button
-        self.get_weather_button = tk.Button(master, text="Get Current Weather", command=self.get_weather_data,
+        self.get_weather_button = tk.Button(master, text="Get Weather", command=self.get_weather_data,
                                             font=("Helvetica", 14))
         self.get_weather_button.pack(padx=10, pady=10)
-
-        # Create Get Forecast button
-        self.get_forecast_button = tk.Button(master, text="Get 3-Day Forecast", command=self.get_forecast,
-                                             font=("Helvetica", 14))
-        self.get_forecast_button.pack(padx=10, pady=10)
 
         # Create weather information display area
         self.weather_info_label = tk.Label(master, text="Weather Information:", font=("Helvetica", 16), fg="blue",
                                            bg="lightblue")
         self.weather_info_label.pack(padx=10, pady=10)
 
-        self.weather_info_text = tk.Text(master, height=10, width=40, font=("Helvetica", 14))
+        self.weather_info_text = tk.Text(master, height=20, width=60, font=("Helvetica", 14))
         self.weather_info_text.pack(padx=10, pady=10)
+
+        # Create temperature unit toggle button
+        self.temp_unit_var = tk.StringVar(value="Celsius")
+        self.temp_unit_button = tk.Button(master, textvariable=self.temp_unit_var, command=self.toggle_temp_unit,
+                                          font=("Helvetica", 14), bg="yellow", activebackground="green")
+        self.temp_unit_button.pack(padx=10, pady=10)
+
+        # Load user preferences from file if it exists
+        if os.path.exists("Settings.txt"):
+            with open("Settings.txt", "r") as f:
+                lines = f.readlines()
+                if len(lines) >= 2:
+                    city = lines[0].strip()
+                    temp_unit = lines[1].strip()
+                    self.city_var.set(city)
+                    self.temp_unit_var.set(temp_unit)
+                    if temp_unit == "Fahrenheit":
+                        self.temp_unit_button.configure(bg="green", activebackground="yellow")
+                    self.get_weather_data()
 
     def get_weather_data(self):
         try:
-            # Get entered city from search box
+            # Get selected city from dropdown list
             city = self.city_var.get()
             city = city.replace(" ", "-")
 
-            # Send request to weather website to get weather data for entered city
-            url = f"https://www.weather-forecast.com/locations/{city}/forecasts/latest"
+            # Set API key and base URL
+            api_key = "d14bd97a2016de74a13dd57deb9157a8"
+            base_url = "http://api.openweathermap.org/data/2.5"
+
+            # Send request to OpenWeatherMap API to get current weather data for selected city
+            url = f"{base_url}/weather?q={city}&appid={api_key}&units=metric"
             response = requests.get(url)
-            soup = BeautifulSoup(response.text, "html.parser")
+            data = response.json()
 
-            # Extract weather data from response
-            temp_td = soup.find("td", class_="b-metar-table__temperature")
-            temp_div = temp_td.find("div", class_="b-metar-table__temperature-value")
-            temp = float(temp_div.find("span", class_="temp").text)
-
-            wind_div = soup.find("div", class_="b-metar-table__wind-detail")
-            wind_speed = float(wind_div.find("span", class_="windp").text)
+            # Extract current weather data from response
+            temp = data["main"]["temp"]
+            wind_speed = data["wind"]["speed"]
+            weather_description = data["weather"][0]["description"]
 
             # Convert temperature to Fahrenheit if necessary
             if self.temp_unit_var.get() == "Fahrenheit":
@@ -80,55 +97,64 @@ class WeatherApp:
             else:
                 wind_speed_icon = "💨"
 
-            # Update weather information display area
-            weather_info = f"City: {city}\nTemperature: {temp_str} {temp_icon}\nWind Speed: {wind_speed} km/h {wind_speed_icon}"
-            self.weather_info_text.delete(1.0, tk.END)
-            self.weather_info_text.insert(tk.END, weather_info)
-        except requests.exceptions.ConnectionError:
-            self.weather_info_text.delete(1.0, tk.END)
-            self.weather_info_text.insert(tk.END, "Error: No internet connection")
+            # Update weather information display area with current weather data
+            weather_info = f"City: {city}\n\nCurrent Weather:\nTemperature: {temp_str} {temp_icon}\nWind Speed: {wind_speed} m/s {wind_speed_icon}\nDescription: {weather_description}\n\n3-Day Forecast:\n"
 
-    def get_forecast(self):
-        try:
-            # Get entered city from search box
-            city = self.city_var.get()
-            city = city.replace(" ", "-")
-
-            # Get location key for entered city
-            location_keys = {
-                "Ankara": "316938",
-                "Izmir": "318290",
-                "Istanbul": "318251",
-                "Antalya": "316939",
-                "Bursa": "317350"
-            }
-            location_key = location_keys[city]
-
-            # Send request to AccuWeather API to get 5-day forecast for entered city
-            url = f"http://dataservice.accuweather.com/forecasts/v1/daily/5day/{location_key}?apikey=A4AwSvoRGPFyj8Oa2cC5w2HqchdSIDcT"
+            # Send request to OpenWeatherMap API to get 3-day forecast data for selected city
+            url = f"{base_url}/forecast?q={city}&appid={api_key}&units=metric"
             response = requests.get(url)
             data = response.json()
 
             # Extract 3-day forecast data from response
-            forecast_data = data["DailyForecasts"][:3]
-            forecast_strs = []
-            for day_data in forecast_data:
-                date_str = day_data["Date"][:10]
-                min_temp = day_data["Temperature"]["Minimum"]["Value"]
-                max_temp = day_data["Temperature"]["Maximum"]["Value"]
-                day_str = f"{date_str}: {min_temp}°F lo - {max_temp}°F hi"
-                forecast_strs.append(day_str)
-            forecast_str = "\n".join(forecast_strs)
+            day_count = 0
+            for forecast in data["list"]:
+                dt_txt = forecast["dt_txt"]
+                if "09:00:00" in dt_txt or "21:00:00" in dt_txt:
+                    temp = forecast["main"]["temp"]
+                    weather_description = forecast["weather"][0]["description"]
 
-            # Update weather information display area
-            weather_info = f"City: {city}\n3-Day Forecast:\n{forecast_str}"
+                    # Convert temperature to Fahrenheit if necessary
+                    if self.temp_unit_var.get() == "Fahrenheit":
+                        temp_fahrenheit = temp * 9 / 5 + 32
+                        temp_str = f"{temp_fahrenheit:.1f}°F"
+                    else:
+                        temp_str = f"{temp:.1f}°C"
+
+                    # Update weather information display area with forecast data
+                    weather_info += f"{dt_txt}: {temp_str}, {weather_description}\n"
+
+                    if "21:00:00" in dt_txt:
+                        day_count += 1
+
+                    if day_count == 3:
+                        break
+
             self.weather_info_text.delete(1.0, tk.END)
             self.weather_info_text.insert(tk.END, weather_info)
         except requests.exceptions.ConnectionError:
             self.weather_info_text.delete(1.0, tk.END)
             self.weather_info_text.insert(tk.END, "Error: No internet connection")
 
+    def toggle_temp_unit(self):
+        # Toggle temperature unit between Celsius and Fahrenheit
+        if self.temp_unit_var.get() == "Celsius":
+            self.temp_unit_var.set("Fahrenheit")
+            self.temp_unit_button.configure(bg="green", activebackground="yellow")
+        else:
+            self.temp_unit_var.set("Celsius")
+            self.temp_unit_button.configure(bg="yellow", activebackground="green")
+
+        # Update weather information display
+        self.get_weather_data()
+
+    def save_preferences(self):
+        # Save user preferences to file
+        with open("Settings.txt", "w") as f:
+            f.write(f"{self.city_var.get()}\n")
+            f.write(f"{self.temp_unit_var.get()}\n")
+
 
 root = tk.Tk()
 app = WeatherApp(root)
+root.protocol("WM_DELETE_WINDOW", lambda: (app.save_preferences(), root.destroy()))
 root.mainloop()
